3bc40ecb4fdc26cd8ba91e32aa21f63cadb31157
Umar Shabazov <malik95i@yandex.ru>
2024-10-29 19:18:34 +0400

    Updated WordByWordTokenizer by adding delimiter to constructor and checking for empty String []/

--- src/main/java/org/example/tokenization/WordByWordTokenizer.java
+++ src/main/java/org/example/tokenization/WordByWordTokenizer.java
@@ -2,11 +2,22 @@ package org.example.tokenization;
 
 public class WordByWordTokenizer implements Tokenizer {
 
+    private final String delimiter;
+
+    public WordByWordTokenizer(String delimiter) {
+        this.delimiter = delimiter != null ? delimiter : "\\P{IsAlphabetic}+";
+    }
+
+    public WordByWordTokenizer() {
+        this("\\P{IsAlphabetic}+");
+    }
+
     @Override
     public String[] tokenize(String text) {
 
-        return text.toLowerCase().split("\\P{IsAlphabetic}+");
-
+        if (text == null || text.isEmpty()) {
+            return new String[0];
+        }
+        return text.toLowerCase().split(delimiter);
     }
-
 }
